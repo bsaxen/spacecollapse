6683a2659538f54f5833fc3ea4f1e0838bafa899
bsaxen <benny.saxen@gmail.com>
2018-09-13 08:44:54 +0200

    Update dipServer.php

--- dipServer/dipServer.php
+++ dipServer/dipServer.php
@@ -1,2 +1,14 @@
-<php
-Data Ip Address
+<?php
+//=============================================
+// File.......: dipServer.php
+// Date.......: 2018-09-
+// Author.....: Benny Saxen
+// Description: Data Ip Server
+//              1. Register new dip connections dip-uri
+//              2. Lookup dip - return uri
+//=============================================
+
+// Register dip connection
+// Format: dipserver.com?dip=1234.1234.1234.1234.1234.1234.1234.1234&uri="spacecollapse.simuino.com/temperature_indoor_kvv32"
+
+?>
